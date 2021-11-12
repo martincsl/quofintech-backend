42e7eb3560abcee6d85deb069df006b7ca1a4f9a
martincsl <martincsl@hotmail.com>
2021-11-11 21:17:15 -0300

    correcciones base de datos

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -11,7 +11,7 @@ module.exports = {
         .join ('sponsors', 'sponsors.sponsorId','=', 'loans.sponsorId')
         .join ('customers', 'customers.customerId','=', 'loans.customerId')
         .where ('loans.sponsorId',sponsorId);
-      console.log(loansInfo);
+      // console.log(loansInfo);
       return response.json(loansInfo);
     } catch (error) {
         next (error);
@@ -22,8 +22,8 @@ module.exports = {
     try {
       const { id } = request.params;
       const sponsorId = request.headers.authorization;
-      console.log(id);
-      console.log(sponsorId);
+      // console.log(id);
+      // console.log(sponsorId);
       const [count] = await connection('loans').count();
       const incident = await connection('loans') // colocar condigo para id nao encontrada
         .where('id', id)
